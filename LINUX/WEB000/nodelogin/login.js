89c2eb53da4c3d50bbaffb7c167d1e06453856d6
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-01 21:14:47 +0800

    done with docker
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- LINUX/WEB000/nodelogin/login.js
+++ LINUX/WEB000/nodelogin/login.js
@@ -4,10 +4,7 @@ const session = require("express-session");
 const path = require("path");
 const { exec } = require("child_process");
 const flag_1 = "sig21CTF{wh0_s4id_1_need_passw0rd_t0_l0g1n}";
-const flag_2 = "sig21CTF{n0t_0nl7_c4t_c4n_v13w_f1l3s}";
-const flag_3 = "sig21CTF{1_though7_1t_w4s_s3cur3d_t0_st0r3_th3_55h_k3y5_h3r3}";
-const flag_4 = "sig21CTF{y0u_ju5t_g0tt4_l34rn_5tuff_0n_y0ur_0wn}";
-
+const port = 3000
 const connection = mysql.createConnection({
   host: "localhost",
   user: "abuyusif",
@@ -28,11 +25,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
 
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.sendFile(path.join(__dirname + "/login.html"));
 });
 
-app.post("/auth", function (request, response) {
+app.post("/auth", (request, response) => {
   let username = request.body.username;
   let password = request.body.password;
   if (username && password) {
@@ -54,7 +51,7 @@ app.post("/auth", function (request, response) {
   }
 });
 
-app.get("/home", function (request, response) {
+app.get("/home", (request, response) => {
   if (request.session.loggedin) {
     response.redirect("/dash");
   } else {
@@ -65,7 +62,7 @@ app.get("/home", function (request, response) {
 
 app.get("/dash", (request, response) => {
   if (request.session.loggedin) {
-    exec(`echo ${flag_1} > flag_1:${flag_1}`, () => {});
+    exec(`echo ${flag_1} > flag_1:${flag_1}`, () => { });
   }
   response.sendFile(path.join(__dirname + "/dashboard.html"));
 });
@@ -78,6 +75,7 @@ const sanitize = (value) => {
     strings: "strings",
     head: "head",
     tail: "tail",
+    rm: "rm"
   };
   return Object.values(blocked).includes(value.split(" ")[0]);
 };
@@ -88,20 +86,21 @@ app.post("/dash", (request, response) => {
     !command
       ? response.send("Enter some command brooo!!!")
       : !sanitize(command)
-      ? exec(command, (error, stdout, stderr) => {
+        ? exec(`/bin/zsh -c "${command}"`, (error, stdout, stderr) => {
+          // ? exec(command, (error, stdout, stderr) => {
           if (error) {
-            // console.log(`error: ${error.message}`)
-            response.end();
+            response.send(`error: ${stderr}`)
+            return
           }
           if (stderr) {
-            // console.log(`error: ${stderr}`)
-            response.end();
+            response.send(`error: ${stderr}`)
+            return
           }
           // console.log(stdout)
           response.send(stdout);
           response.end();
         })
-      : response.send(
+        : response.send(
           `<b>${command.split(" ")[0]}</b> is blacklisted by the admin :(`
         );
   } else {
@@ -110,4 +109,4 @@ app.post("/dash", (request, response) => {
   }
 });
 
-app.listen(3000);
+app.listen(port);
