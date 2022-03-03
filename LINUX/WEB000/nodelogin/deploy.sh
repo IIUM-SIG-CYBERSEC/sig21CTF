739fa7b7963784998f04b21b330f9b03ad086ffc
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-03 23:37:25 +0800

    nodelogin: delete all dir
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- LINUX/WEB000/nodelogin/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# docker kill $(docker ps -q) 2>/dev/null
-image_name="sig21ctf_nodeapp"
-docker build . -t $image_name
-docker run --net=host -d $image_name:latest
-# container=`docker ps | grep $image_name | awk '{print$1}'| tail -n 1`
-# echo $container
-# docker exec -it $container /bin/bash
--- LINUX/WEB000/nodelogin/wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -m
-
-node /var/www/html/sig21CTF/webapp/login.js &
-echo "kLqeRS87Z3LUWdchAMTs" | su root -c "/usr/sbin/sshd -D" 2&>/dev/null
-
-fg %1
