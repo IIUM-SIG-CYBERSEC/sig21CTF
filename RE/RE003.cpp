9b8c5e656846b905d3062b21f1b5185569dc1ac2
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-05 01:36:24 +0800

    add one last challenge for re
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

---
+++ RE/RE003.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int value = 20212022;
+int main () {
+    int temp;
+    cin >> temp;
+    temp == value ? cout << "SUCESS"  : cout << "FAILURE!!!";
+    return 0;
+}
+// dca92a085ebc8a1a668c08a4954e1dd0  -
