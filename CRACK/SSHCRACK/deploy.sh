3c221d982918690c59a8f610e65f86b25eaae79f
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-05 13:06:16 +0000

    misc: kill old crack
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- CRACK/SSHCRACK/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-image_name="crackssh"
-old_image=`docker ps | grep $image_name | awk '{print$1}'`
-docker kill $old_image
-docker build . -t $image_name
-docker run --net=host -d $image_name
-
-
