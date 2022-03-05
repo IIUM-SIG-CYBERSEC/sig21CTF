a65b8f6016fe8d0b01f73a2109a2b80fada007ed
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-05 20:41:19 +0800

    sig21CTF: fix all docker deploy
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- LINUX/LIN002/deploy.sh
+++ LINUX/LIN002/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# docker kill $(docker ps -q) 2>/dev/null
 image_name="lin0001"
+old_image=`docker ps | grep $image_name | awk '{print$1}'`
+docker kill $old_image
 docker build . -t $image_name
 docker run -p 1337:1337 --expose=1337 -d $image_name
 
-# container=`docker ps | grep $image_name | awk '{print$1}'| tail -n 1`
-# echo $container
-# docker exec -it $container /bin/bash
--- LINUX/nodelogin/deploy.sh
+++ LINUX/nodelogin/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# docker kill $(docker ps -q) 2>/dev/null
 image_name="sig21ctf_nodeapp"
+old_image=`docker ps | grep $image_name | awk '{print$1}'`
+docker kill $old_image
 docker build . -t $image_name
 docker run --net=host -d $image_name:latest
-# container=`docker ps | grep $image_name | awk '{print$1}'| tail -n 1`
-# echo $container
-# docker exec -it $container /bin/bash
