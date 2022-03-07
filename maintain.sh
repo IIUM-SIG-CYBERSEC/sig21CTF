7a6dbd014d7b5d7eb126ed02ebd7d90d5a5830ce
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-07 20:58:05 +0800

    cron: passwd?
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- maintain.sh
+++ maintain.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # create account for the teams
 touch passwd_file
-# root_passwd="Zz2@[z=GREfS{Wq-"
 for (( i = 1; i < 13; i++ )); do
  #   sudo useradd -g ctf-users -s /bin/bash -m group-$i
  PASS=`cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1`
