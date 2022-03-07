d2d616bc0be25369c66a556dbaf58031a5c270b4
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-07 05:47:54 +0000

    gitignore: add passwd files
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- LINUX/check.sh
+++ LINUX/check.sh
@@ -3,6 +3,6 @@
 port=$1
 curl localhost:$port 2>/dev/null
 if [[ $? -ne 0 ]]; then
-    /bin/bash /home/blackdracula/ctf/sig21CTF/LINUX/nodelogin/deploy.sh $port 2&>/dev/null
+    /bin/bash /home/abuyusif/sig21CTF/LINUX/nodelogin/deploy.sh $port 2&>/dev/null
     exit
 fi
