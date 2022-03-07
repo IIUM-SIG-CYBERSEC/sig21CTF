d4ec20872abacfb80dd47b40eee051edd6ea7378
blackdracula18 <abubakaryusuf951@gmail.com>
2022-03-07 05:41:56 +0000

    nodelogin: fix dirs
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- LINUX/nodelogin/deploy.sh
+++ LINUX/nodelogin/deploy.sh
@@ -3,5 +3,6 @@
 image_name="sig21ctf_nodeapp"
 old_image=`docker ps | grep $image_name | awk '{print$1}'`
 docker kill $old_image
-docker build /home/blackdracula/ctf/sig21CTF/LINUX/nodelogin -t $image_name
+# docker build /home/blackdracula/ctf/sig21CTF/LINUX/nodelogin -t $image_name
+docker build /home/abuyusif/sig21CTF/LINUX/nodelogin -t $image_name
 docker run --net=host -d $image_name:latest
